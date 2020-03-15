2a1a1bfdc91eb786936ccf01d6a11f44e6f5d381
nebelgrau <nebelgrau@gmx.com>
2020-03-15 10:54:44 +0100

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-//! conway's game of life DRAFT
+//! Conway's Game of Life
 //! 
-//! very slow
+//! original MicroPython code ported to STM32F0xx board (this code is for STM32F030F4P6)
 //! 
-//! can fit 64x64 grid using single bits to represent pixels
+//! improved to store pixels as single bits, i.e. one byte is 8 pixels
+//! 
+//! the code currently resets after a 1000 generations
+//! 
+//! the grid "dies" after less than 600 generations
+//! leaving only static or oscillating debris
 //! 
 //! next steps: use ADC to get a different RNG seed each time
 //! 
@@ -33,16 +38,17 @@ use embedded_graphics::{
     };
 
 use core::fmt;
-//use core::fmt::Write;
 use arrayvec::ArrayString;
 
 struct Pixel {
     
     byteidx: u16,
-    bitidx: u8,
+    bitidx: i8,
     value: u8,
 }
 
+static WX: i8 = 64; // grid width
+static HY: i8 = 64; // grid height
 
 #[entry]
 fn main() -> ! {
@@ -51,7 +57,6 @@ fn main() -> ! {
         
         cortex_m::interrupt::free(move |cs| {
 
-        // let mut rcc = p.RCC.configure().sysclk(8.mhz()).freeze(&mut p.FLASH);
         let mut rcc = p.RCC.configure().sysclk(48.mhz()).freeze(&mut p.FLASH);
         
         let gpioa = p.GPIOA.split(&mut rcc);
@@ -63,94 +68,81 @@ fn main() -> ! {
         
         disp.init().unwrap();
         
-        let w: u8 = 64;
-        let h: u8 = 64;   
-
         
-
         loop {
 
             let mut rng = SmallRng::seed_from_u64(0x0101_0808_0303_0909);
 
-            let mut matrix = [0u8; 512]; 
+            let mut buffer = [0u8; 512]; 
 
             let text_style = TextStyleBuilder::new(Font6x8).text_color(BinaryColor::On).build();
 
-            // generate first random matrix
+            // generate first random grid
 
-            rng.fill_bytes(&mut matrix);
+            rng.fill_bytes(&mut buffer);
 
             // display it
         
-            for x in 0..w {
-                for y in 0..h {
-                    let pixel = pixelgetter(x,y,w,matrix);
+            for x in 0..WX {
+                for y in 0..HY {
+                    let pixel = pixelgetter(x,y,buffer);
                     disp.set_pixel(x as u32,y as u32,pixel.value);
                 }
             }
 
-            let mut format_buf = ArrayString::<[u8; 8]>::new(); // create a buffer for the counter text
+            let mut text_buf = ArrayString::<[u8; 8]>::new(); // create a buffer for the counter text
 
             let mut gen: u16 = 0; // generation counter
             
-            format_gen(&mut format_buf, gen);
+            counter(&mut text_buf, gen);
 
-            Text::new(format_buf.as_str(), Point::new(80, 0)).into_styled(text_style).draw(&mut disp);
+            Text::new(text_buf.as_str(), Point::new(80, 0)).into_styled(text_style).draw(&mut disp);
 
             disp.flush().unwrap();
 
-            while gen < 1000 {
+            while gen < 1001 {
 
-                let w: u8 = 64;
-                let h: u8 = 64;
+                // let w: u8 = 64;
+                // let h: u8 = 64;
         
                 // clean up the number area of the counter
         
-                for m in 110..128 {
+                for m in 100..128 {
                     for n in 0..8 {
                         disp.set_pixel(m, n, 0);
                     }
                 }
         
         
-                let mut format_buf = ArrayString::<[u8; 8]>::new();
+                let mut text_buf = ArrayString::<[u8; 8]>::new();
         
                 gen += 1;
         
-                format_gen(&mut format_buf, gen);
+                counter(&mut text_buf, gen);
         
-                Text::new(format_buf.as_str(), Point::new(80, 0)).into_styled(text_style).draw(&mut disp);
+                Text::new(text_buf.as_str(), Point::new(80, 0)).into_styled(text_style).draw(&mut disp);
         
                 
-                matrix = matrix_evo(matrix, 64, w);
+                buffer = matrix_evo(buffer);
         
-                for x in 0..w {
-                    for y in 0..h {
-                        let pixel = pixelgetter(x,y,w,matrix);
+                for x in 0..WX {
+                    for y in 0..HY {
+                        let pixel = pixelgetter(x,y,buffer);
                         disp.set_pixel(x as u32,y as u32,pixel.value);
+                        }
                     }
-                }
                 
-        
-        
                 disp.flush().unwrap();
         
-        
-        
-        
+                }
+
             }
-
-
-
-        }
-
        
-    });
+        });
     
-}
-
-    loop {continue;}
-    
+    }
+        loop {continue;}
+  
 }
 
 
@@ -174,37 +166,35 @@ fn evo(cell: u8, neighbors: u8) -> u8 {
             new_cell = 1 
             }
         }
-    
         return new_cell;
-        
     }
  
     
 // generate a new matrix made of bytes, based on the cells evolution
 
-fn matrix_evo(matrix: [u8; 512], size: i8, w: u8) -> [u8; 512] {
+fn matrix_evo(buffer: [u8; 512]) -> [u8; 512] {
     
     //must use signed integers otherwise can't do the -1/+1 operation 
     
-    let mut new_matrix = [0u8;512];
+    let mut new_buffer = [0u8;512];
 
     let mut new_cell: u8 = 0;
     let mut cell: u8 = 0;
 
-    for x in 0..size {
-        for y in 0..size {
+    for x in 0..WX {
+        for y in 0..HY {
             
-            let pixel = pixelgetter(x as u8,y as u8,w,matrix);
+            let pixel = pixelgetter(x,y, buffer);
 
             let mut neighbors: u8 = 0;
             
             for n in -1..2 {
                 for m in -1..2 {
-                    if x + n < 0 || y + m < 0 || (x + n) > (size - 1) || (y + m) > (size - 1) || ((m == 0) && (n == 0)) {
+                    if x + n < 0 || y + m < 0 || (x + n) > (WX - 1) || (y + m) > (WX - 1) || ((m == 0) && (n == 0)) {
                         continue // do nothing, that is neighbors stays as it is
                     }
                     else {
-                        let new_neighbor = pixelgetter((x+n) as u8, (y+m) as u8, w, matrix);
+                        let new_neighbor = pixelgetter(x+n, y+m, buffer);
                         neighbors += new_neighbor.value;
                     }
                     
@@ -215,26 +205,16 @@ fn matrix_evo(matrix: [u8; 512], size: i8, w: u8) -> [u8; 512] {
         
 
         if new_cell == 0 {
-            new_matrix[pixel.byteidx as usize] &= !(1 << (7-pixel.bitidx));
+            new_buffer[pixel.byteidx as usize] &= !(1 << (7-pixel.bitidx)); // clear bit to 0
         } 
         else {
-            new_matrix[pixel.byteidx as usize] |= 1 << (7-pixel.bitidx);
+            new_buffer[pixel.byteidx as usize] |= 1 << (7-pixel.bitidx); // set bit to 1
         }
 
 
         }
     }
-    return new_matrix;
-
-}
-
-fn format_gen(buf: &mut ArrayString<[u8; 8]>, gen: u16) {   
-    
-    let singles = gen%10;
-    let tens = (gen/10)%10;
-    let hundreds = (gen/100)%10;
-            
-    fmt::write(buf, format_args!("Gen: {}{}{}", hundreds as u8, tens as u8, singles as u8)).unwrap();
+    return new_buffer;
 
 }
 
@@ -242,10 +222,10 @@ fn format_gen(buf: &mut ArrayString<[u8; 8]>, gen: u16) {
 // helper function to get the binary value of (x,y) pixel in a 1-D array of bytes
 // needs the screen width as a parameter to correctly identify the bytes of each line
 
-fn pixelgetter(x: u8, y: u8, w: u8, array: [u8; 512]) -> Pixel {
-    let byteidx: u16 = y as u16 *(w as u16 /8) + x as u16/8;
-    let bitidx: u8 = x%8;
-    let byte: u8 = array[byteidx as usize];
+fn pixelgetter(x: i8, y: i8, buffer: [u8; 512]) -> Pixel {
+    let byteidx: u16 = y as u16 *(WX as u16 /8) + x as u16/8;
+    let bitidx: i8 = x%8;
+    let byte: u8 = buffer[byteidx as usize];
     let pixelval: u8 = (byte/(2_u8.pow((7-bitidx) as u32)))%2;
     return Pixel {
         byteidx: byteidx,
@@ -254,3 +234,16 @@ fn pixelgetter(x: u8, y: u8, w: u8, array: [u8; 512]) -> Pixel {
     };
 }
 
+
+// helper function for the generation counter
+
+fn counter(buf: &mut ArrayString<[u8; 8]>, gen: u16) {   
+    
+    let singles = gen%10;
+    let tens = (gen/10)%10;
+    let hundreds = (gen/100)%10;
+    let thousands = (gen/1000)%10;
+            
+    fmt::write(buf, format_args!("Gen:{}{}{}{}", thousands as u8, hundreds as u8, tens as u8, singles as u8)).unwrap();
+
+}
